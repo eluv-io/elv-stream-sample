5aeb7daeeb547b9da497503728f337cce8d7d178
Kevin Talmadge <kevin@eluv.io>
2019-08-12 17:23:45 -0700

    Add buffer, latency and bandwidth graphs

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,386 +1,165 @@
 import React from "react";
 import PropTypes from "prop-types";
 import HLSPlayer from "../../node_modules/hls.js/dist/hls";
+import {BufferHelper} from "../../node_modules/hls.js/src/utils/buffer-helper";
+
 import DashJS from "dashjs";
 import URI from "urijs";
-import { VictoryLine, VictoryChart,VictoryAxis, VictoryTheme, VictoryScatter, VictoryLegend, VictoryLabel } from "victory";
-import {CalculateHTTPMetrics} from "../Utils.js";
-
-class LineGraph extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      chartWidth: 0,
-      axisFontSize: 10,
-      /* to-do add other specifics for styling */
-      // lineSize: 5,
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      chartWidth: window.innerWidth
-    });
-    window.addEventListener("resize", this.updateDimensions.bind(this));
-  }
-
-  /* clear event listener */
-  componentWillUnmount(){
-    window.removeEventListener("resize", this.updateDimensions.bind(this));
-  }
-
-  updateDimensions(event) {
-    // let size = "4vw";
-    let size = "8px";
-    this.setState({
-      chartWidth: event.target.innerWidth,
-      axisFontSize: size,
-    });
-  }
-
-  render(){
-
-    return(
-
-      <svg viewBox={"0 0" + " "+ this.state.chartWidth +" " + "350"}  preserveAspectRatio="none" width="100%">
-
-        <VictoryChart
-          theme={VictoryTheme.material}
-          maxDomain={{ y: this.props.max }}
-          minDomain={{ y: 0 }}
-          standalone={false}
-          width={this.state.chartWidth}
-          height={350}
-        >
-
-          <VictoryLegend
-            height={100}
-            width={100}
-            centerTitle
-            style={{title: {fontSize: 8 } }}
-            colorScale = {[this.props.color]}
-            data={[
-              {name: this.props.name, symbol: { type: "square"}},
-            ]}
-          />
-
-          <VictoryAxis
-            // tickValues specifies both the number of ticks and where
-            // they are placed on the axis
-            label="Time (s)"
-            axisLabelComponent={<VictoryLabel dy={20}/>}
-
-            style={{tickLabels: { fill: "#ceded7", fontSize: this.state.axisFontSize }}}
-          />
-          <VictoryAxis
-            dependentAxis
-            // tickFormat specifies how ticks should be displayed
-            style={{tickLabels: { fill: "#ceded7", fontSize: this.state.axisFontSize }}}
-          />
-
-          <VictoryLine
-            style={{
-              data: { stroke: this.props.color, strokeWidth: 1 },
-            }}
-            data={this.props.data}
-          />
-
-          <VictoryScatter data={this.props.data}
-            size={.75}
-            style={{ data: { fill: this.props.color } }}
-          />
-        </VictoryChart>
-
-      </svg>
-
-    );
-  }
-}
+import Graph from "./Graph";
 
 class Video extends React.Component {
   constructor(props) {
     super(props);
 
-    //set initial state here
-    this.state = this.GetInitialState();
-
-    this.metricsTimer = null;
-    this.streamInfo;
-    this.sessionStartTime = 0;
-    this.maxPointsToChart = 30;
-
-    this.InitializeVideo = this.InitializeVideo.bind(this);
-    this.SetupDashCharts = this.SetupDashCharts.bind(this);
-    this.SetupHLSCharts = this.SetupHLSCharts.bind(this);
-    this.UpdateMetrics = this.UpdateMetrics.bind(this);
-    this.StopMetricsInterval = this.StopMetricsInterval.bind(this);
-    this.GetTimeForPlot = this.GetTimeForPlot.bind(this);
-  }
-
-  GetInitialState(){
-    const initialState = {
-      //for handling video data
-      chartsEnabled: false,
+    this.state = {
       bufferData: [],
       bitrateData: [],
       latencyData: [],
-
-      buffer:         {color: "#00589d", label: "Video Buffer Level"},
-      bitrate:        {color: "#ff7900", label: "Video Bitrate (kbps)"},
-      latency:        {color: "#329d61", label: "Video Latency (ms)"},
+      graphWindowSize: 20,
+      sampleInterval: 250
     };
-    return initialState;
+
+    this.metricsInterval = null;
+
+    this.InitializeVideo = this.InitializeVideo.bind(this);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.metricsInterval);
+    this.metricsInterval = undefined;
   }
 
   InitializeVideo(video) {
     if(!video) { return; }
 
-    let videoUrl = this.props.playoutOptions.playoutUrl;
+    let playoutUrl = this.props.playoutOptions.playoutUrl;
     if(this.props.videoType === "hls") {
-      videoUrl = URI(videoUrl).addSearch("player_profile", "hls-js").toString();
-
-      const player = new HLSPlayer();
-      player.loadSource(videoUrl);
-      player.attachMedia(video);
-      SetupHLSCharts(player);
-      this.setState({chartsEnabled: true});
+      this.InitializeHLS(video, playoutUrl);
     } else {
-      const player = DashJS.MediaPlayer().create();
+      this.InitializeDash(video, playoutUrl);
+    }
+  }
 
-      if (this.props.drm === "widevine") {
-        const widevineUrl = this.props.playoutOptions.drms.widevine.licenseServers[0];
+  InitializeHLS(video, playoutUrl) {
+    playoutUrl = URI(playoutUrl).addSearch("player_profile", "hls-js").toString();
 
-        player.setProtectionData({
-          "com.widevine.alpha": {
-            "serverURL": widevineUrl,
-            "httpRequestHeaders": {
-              "Authorization": `Bearer ${this.props.authToken}`
-            },
-            "withCredentials": false
-          }
-        });
+    const player = new HLSPlayer();
+    const initialTime = performance.now();
+
+    player.loadSource(playoutUrl);
+    player.attachMedia(video);
+
+    this.metricsInterval = setInterval(() => {
+      const currentTime = (performance.now() - initialTime) / 1000;
+      const stats = player.streamController.stats;
+
+      if(!stats) { return; }
+
+      const bufferInfo = BufferHelper.bufferInfo(video, video.currentTime, 0);
+
+      this.setState({
+        bitrateData: this.state.bitrateData.concat({
+          x: currentTime,
+          y: (Math.round((8 * stats.total) / (stats.tbuffered - stats.trequest)) / 10) || 0
+        }),
+        latencyData: this.state.latencyData.concat({
+          x: currentTime,
+          y: (stats.tfirst - stats.trequest) / 1000
+        }),
+        bufferData: this.state.bufferData.concat({
+          x: currentTime,
+          y: (bufferInfo.len) || 0
+        })
+      });
+    }, this.state.sampleInterval);
+  }
+
+  InitializeDash(video, playoutUrl) {
+    const player = DashJS.MediaPlayer().create();
+
+    if (this.props.drm === "widevine") {
+      const widevineUrl = this.props.playoutOptions.drms.widevine.licenseServers[0];
+
+      player.setProtectionData({
+        "com.widevine.alpha": {
+          "serverURL": widevineUrl,
+          "httpRequestHeaders": {
+            "Authorization": `Bearer ${this.props.authToken}`
+          },
+          "withCredentials": false
+        }
+      });
+    }
+
+    // Subtitles are enabled by default - disable them
+    player.on(
+      DashJS.MediaPlayer.events.CAN_PLAY,
+      () => player.setTextTrack(-1)
+    );
+
+    const initialTime = performance.now();
+
+    this.metricsInterval = setInterval(() => {
+      let dashMetrics = player.getDashMetrics();
+      let dashAdapter = player.getDashAdapter();
+
+      let bufferLevel, bitrate, repSwitch, periodIdx;
+
+      if(dashMetrics && this.streamInfo) {
+        periodIdx = this.streamInfo.index;
+        repSwitch = dashMetrics.getCurrentRepresentationSwitch("video", true);
+        bufferLevel = dashMetrics.getCurrentBufferLevel("video", true);
+        bitrate = repSwitch ? Math.round(dashAdapter.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000) : 0;
       }
 
-      // Subtitles are enabled by default - disable them
-      player.on(
-        DashJS.MediaPlayer.events.CAN_PLAY,
-        () => player.setTextTrack(-1)
-      );
+      const requests = dashMetrics.getHttpRequests("video");
+      const requestWindow =
+        requests
+          .slice(-20)
+          .filter(req =>
+            req.responsecode >= 200 &&
+            req.responsecode < 300 &&
+            req.type === "MediaSegment" &&
+            req._stream === "video" &&
+            !!req._mediaduration
+          )
+          .slice(-2);
 
-      player.initialize(video, videoUrl);
-      this.sessionStartTime = new Date().getTime() / 1000;
-      this.SetupDashCharts(player);
-      this.setState({chartsEnabled: true});
-    }
-  }
+      let latency = 0;
+      if(requestWindow.length > 0) {
+        const latencyTimes = requestWindow.map(req =>
+          Math.abs(req.tresponse.getTime() - req.trequest.getTime()) / 1000
+        );
 
-  /* re-set timer */
-  componentWillUnmount(){
-    //clear timeout
-    this.StopMetricsInterval();
-  }
+        latency = latencyTimes.reduce((l, r) => l + r) / latencyTimes.length;
+      }
 
-  SetupDashCharts(player){
-    /* preserve the this context */
-    let component = this;
+      const currentTime = (performance.now() - initialTime) / 1000;
 
-    player.on(dashjs.MediaPlayer.events.STREAM_INITIALIZED, function(){
-      component.StopMetricsInterval();
-      // we set a timer for each second to update metrics
-      component.metricsTimer = setInterval(function(){
-        component.UpdateMetrics("video", player);
-      }, 1000);
-
-    });
-    player.on(dashjs.MediaPlayer.events.PERIOD_SWITCH_COMPLETED, function (e) { /* jshint ignore:line */
-      component.streamInfo = e.toStreamInfo;
-    });
-  }
-
-  SetupHLSCharts(hls){
-
-    let events = {
-      t0: performance.now(),
-      bitrate: [],
-      latency: [],
-      bufferLevel: [],
-      time: 0,
-    };
-
-    let currentBitrate = 0, currentLatency = 0, currentBufferLevel = 0;
-
-    hls.on(HLSPlayer.Events.FRAG_BUFFERED, function(event, data) {
-
-      /* preferred approach is to trim array depending on time */
-
-      // data.stats.total -> number of bytes processed, and bitrate is the amount of time it took to process that request
-      events.bitrate.push({
-        bitrate: Math.round(8*data.stats.total/(data.stats.tbuffered - data.stats.trequest)),
-        time: performance.now() - events.t0,
+      this.setState({
+        bufferData: this.state.bufferData.concat({
+          x: currentTime,
+          y: bufferLevel,
+        }),
+        latencyData: this.state.latencyData.concat({
+          x: currentTime,
+          y: latency
+        }),
+        bitrateData: this.state.bitrateData.concat({
+          x: currentTime,
+          y: bitrate
+        })
       });
+    }, this.state.sampleInterval);
 
-      events.latency.push({
-        latency: (data.stats.tfirst - data.stats.trequest)/1000,
-        time: performance.now() - events.t0,
-      });
-
-      /* don't think this calculates the correct buffer */
-      events.bufferLevel.push({
-        bufferLevel : data.stats.tbuffered - data.stats.tparsed,
-        time: performance.now() - events.t0,
-      });
+    player.on(DashJS.MediaPlayer.events.PERIOD_SWITCH_COMPLETED, (e) => {
+      this.streamInfo = e.toStreamInfo;
     });
 
-    /* TO-DO: check other event listeners that could affect metrics */
-
-    // hls.on(HLSPlayer.Events.LEVEL_SWITCHING, function(event, data) {
-    // events.level.push({
-    //   time   : performance.now() - events.t0,
-    //   id     : data.level,
-    //   bitrate: Math.round(hls.levels[data.level].bitrate/1000)
-    // });
-    // trimEventHistory();
-    // updateLevelInfo();
-    // });
-
-    /* every second, get the averages of the data in that timeframe */
-
-    this.StopMetricsInterval();
-
-    let prevTimeInterval = 0;
-    let component = this;
-
-    this.metricsTimer = setInterval(function(){
-      let time = performance.now() - events.t0;
-      let bitCount = 0, latCount = 0, bufCount = 0;
-
-      events.bitrate.forEach(function(element){
-        if((element.time <= time) && (element.time>prevTimeInterval)){
-          currentBitrate += element.bitrate;
-          bitCount++;
-        }else{
-          bitCount = 1;
-        }
-      });
-
-      events.latency.forEach(function(element){
-        if((element.time <= time) && (element.time>prevTimeInterval)){
-          currentLatency += element.latency;
-          latCount++;
-        }else{
-          latCount = 1;
-        }
-      });
-
-      events.bufferLevel.forEach(function(element){
-        if((element.time <= time) && (element.time>prevTimeInterval)){
-          currentBufferLevel += element.bufferLevel;
-          bufCount++;
-        }else{
-          bufCount = 1;
-        }
-      });
-
-      currentBitrate = currentBitrate/bitCount;
-      currentBufferLevel = currentBufferLevel/bufCount;
-      currentLatency = currentLatency/latCount;
-      prevTimeInterval = time;
-
-      component.setState(state => {
-        const bufferData = state.bufferData.concat(currentBufferLevel);
-        const latencyData = state.latencyData.concat(currentLatency);
-        const bitrateData = state.bitrateData.concat(currentBitrate);
-
-        return {
-          bufferData,
-          latencyData,
-          bitrateData
-        };
-      });
-
-      currentBitrate = 0, currentLatency = 0, currentBufferLevel = 0;
-    }, 1000);
-
-  }
-
-  /* on restart */
-  StopMetricsInterval() {
-    if (this.metricsTimer) {
-      clearInterval(this.metricsTimer);
-      this.metricsTimer = null;
-    }
-  }
-
-  GetTimeForPlot(){
-    let now = new Date().getTime() / 1000;
-    return Math.max(now - this.sessionStartTime, 0);
-  }
-
-  /* updates Dash metrics */
-  UpdateMetrics(type, player){
-    let dashMetrics = player.getDashMetrics();
-    let dashAdapter = player.getDashAdapter();
-
-    let bufferLevel, bitrate, latency, repSwitch, periodIdx;
-
-    if (dashMetrics && this.streamInfo) {
-      periodIdx = this.streamInfo.index;
-      repSwitch = dashMetrics.getCurrentRepresentationSwitch(type, true);
-      bufferLevel = dashMetrics.getCurrentBufferLevel(type, true);
-      bitrate = repSwitch ? Math.round(dashAdapter.getBandwidthForRepresentation(repSwitch.to, periodIdx) / 1000) : NaN;
-    }
-
-    let httpMetrics = CalculateHTTPMetrics(type, dashMetrics.getHttpRequests(type));
-    //time isn't being used since we're updating every 1 second anyways
-    // let time = this.GetTimeForPlot();
-
-    latency = parseFloat(httpMetrics.latency[type].average.toFixed(2));
-
-    this.setState(state => {
-      const bufferData = state.bufferData.concat(bufferLevel);
-      const latencyData = state.latencyData.concat(latency);
-      const bitrateData = state.bitrateData.concat(bitrate);
-
-      // const bufferData = state.bufferData.concat({x: time, y: bufferLevel});
-      // const latencyData = state.latencyData.concat({x: time, y: latency});
-      // const bitrateData = state.bitrateData.concat({x: time, y: bitrate});
-
-      return {
-        bufferData,
-        latencyData,
-        bitrateData
-      };
-    });
+    player.initialize(video, playoutUrl);
   }
 
   render() {
-
-    let el = null;
-    if(this.state.chartsEnabled){
-
-      let bufferData = this.state.bufferData;
-      let latencyData = this.state.latencyData;
-      let bitrateData = this.state.bitrateData;
-
-      let bufferMax = Math.max(...bufferData)+5;
-      let latencyMax = Math.max(...latencyData)+.05;
-      let bitrateMax = Math.max(...bitrateData)+1000;
-
-      el = (
-        <div>
-
-          <LineGraph data = {bufferData} color = {this.state.buffer.color} max = {bufferMax} name = {this.state.buffer.label}  />
-
-          <LineGraph data = {latencyData} color = {this.state.latency.color} max = {latencyMax} name = {this.state.latency.label} />
-
-          <LineGraph data = {bitrateData} color = {this.state.bitrate.color} max = {bitrateMax} name = {this.state.bitrate.label} />
-
-        </div>
-      );
-
-    }
-
     return (
       <div className="video-container">
         <h1>{this.props.metadata.name}</h1>
@@ -393,11 +172,27 @@ class Video extends React.Component {
           controls={true}
           preload="auto"
         />
-
-        <div>{el}</div>
-
+        <div className="graphs-container">
+          <Graph
+            name="Buffer Level (s)"
+            data={this.state.bufferData}
+            color={"#00589d"}
+            windowSize={this.state.graphWindowSize}
+          />
+          <Graph
+            name="Latency (ms)"
+            data={this.state.latencyData}
+            color={"#329d61"}
+            windowSize={this.state.graphWindowSize}
+          />
+          <Graph
+            name="Bitrate (kbps)"
+            data={this.state.bitrateData}
+            color={"#ff7900"}
+            windowSize={this.state.graphWindowSize}
+          />
+        </div>
       </div>
-
     );
   }
 }
